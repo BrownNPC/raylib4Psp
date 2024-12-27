32b9d10da50a70c1ea13c7189c1568da23dbd048
Linus_tt <100426634+BrownNPC@users.noreply.github.com>
2024-12-27 15:58:13 +0500

    Update pspbuild.sh

--- pspbuild.sh
+++ pspbuild.sh
@@ -2,7 +2,7 @@
 ## Check if $PSPDEV is set.
 if test ! $PSPDEV; then { echo "ERROR: Set \$PSPDEV installing pspdev toolchain and pspsdk before continuing."; exit 1; } fi
 
-git clone https://github.com/raylib4Consoles/raylib -b raylib4Consoles
+git clone https://github.com/BrownNpc/raylib -b raylib4Consoles
 cd raylib/src
 make PLATFORM=PLATFORM_PSP
 make PLATFORM=PLATFORM_PSP install
